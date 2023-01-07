c8fea2a6a244c85797dd79582435084dcad9e1c1
DGeorge <dalton.george94@outlook.com>
2023-01-07 14:03:01 -0600

    update server.py

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, redirect
 import csv
+import os
+
+
+filename = os.path.join(os.path.dirname(__file__), 'database.csv')
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,12 +15,12 @@ def html_page(page_name):
     return render_template(page_name)
 
 def write_to_csv(data):
-    with open('database.csv', mode='a') as database2:
+    with open(filename, mode='a') as database2:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
         csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(email,subject,message)
+        csv_writer.writerow([email,subject,message])
         
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
